4e4e85c8449e1132a0ad8ebe124a3b6baf212026
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2021-12-18 19:16:47 -0500

    lint

--- lib/slack_webhook_logger/formatter.rb
+++ lib/slack_webhook_logger/formatter.rb
@@ -52,8 +52,8 @@ module SlackWebhookLogger
           {
             type: 'section',
             text: {
-              "type": 'plain_text',
-              "text": text
+              type: 'plain_text',
+              text: text
             }
           }
         ]
